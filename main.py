9da8daab6e1a32638a53490ae8dcd37525a2de79
Lav Pareva <lavpareva9@gmail.com>
2026-06-17 15:57:25 +0530

    rtc configuration reverted back

--- main.py
+++ main.py
@@ -201,12 +201,7 @@ def main():
             key="exercise-analysis",
             mode=WebRtcMode.SENDRECV,
             video_processor_factory=VideoProcessorClass,
-            rtc_configuration={
-    "iceServers": [
-        {"urls": "stun:stun.l.google.com:19302"},
-        {"urls": "stun:stun1.l.google.com:19302"}
-    ]
-},
+            rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
             media_stream_constraints={
                 "video": True,
                 "audio": False
